66a6d349ce2193bfd00999a4e632643789717205
nexstrem2 <jobrentransportes195@gmail.com>
2026-02-17 19:24:04 -0300

    Update supabase.ts

--- services/supabase.ts
+++ services/supabase.ts
@@ -1,7 +1,7 @@
 
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = https://mvukroggvzdzvydmykdo.supabase.co';
-const supabaseAnonKey = sb_publishable_ELsT6Q9FEci05sNBO4cfxA_cJNP_Xqt';
+const supabaseUrl = https://mvukroggvzdzvydmykdo.supabase.co ';
+const supabaseAnonKey = sb_publishable_ELsT6Q9FEci05sNBO4cfxA_cJNP_Xqt ';
   
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
