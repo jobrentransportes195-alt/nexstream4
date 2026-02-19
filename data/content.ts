d6c540180dc841c85e372c3af28f685268f86312
nexstrem2 <jobrentransportes195@gmail.com>
2026-02-19 18:15:15 -0300

    Atualizar o content.ts

--- data/content.ts
+++ data/content.ts
@@ -2,14 +2,14 @@ export const content = [
   {
     title: "Avatar",
     category: "Filmes",
-    image: "https://image.tmdb.org/t/p/w500/jRXYjXNq0Cs2TcJjLkki24MLp7u.jpg",
+    image: "https://image.tmdb.org/t/p/w500/6EiRUJpuoeQPghrs3YNktfnqOVh.jpg",
     video: "https://www.w3schools.com/html/mov_bbb.mp4",
     rating: 5
   },
   {
     title: "Breaking Bad",
     category: "Séries",
-    image: "https://image.tmdb.org/t/p/w500/eSzpy96DwBujGFj0xMbXBcGcfxX.jpg",
+    image: "https://image.tmdb.org/t/p/w500/ggFHVNu6YYI5L9pCfOacjizRGt.jpg",
     video: "https://www.w3schools.com/html/movie.mp4",
     rating: 5
   }
