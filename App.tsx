dc15e3443c1b802de8f2d5de2847570b07057b5a
nexstrem2 <jobrentransportes195@gmail.com>
2026-02-19 19:23:54 -0300

    Atualizar o App.tsx

--- App.tsx
+++ App.tsx
@@ -2,16 +2,14 @@ import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import MoviePage from "./components/MoviePage";
+import Favorites from "./components/Favorites";
+import Profile from "./components/Profile";
+import AdminPanel from "./components/AdminPanel";
 
 export default function App() {
-
-  const handleLogout = () => {
-    alert("Logout realizado");
-  };
-
   return (
     <>
-      <Header onLogout={handleLogout} />
+      <Header onLogout={() => alert("Logout")} />
 
       <Routes>
         <Route path="/" element={<Home category="Home" />} />
@@ -19,6 +17,9 @@ export default function App() {
         <Route path="/series" element={<Home category="Séries" />} />
         <Route path="/tv" element={<Home category="TV" />} />
         <Route path="/filme/:id" element={<MoviePage />} />
+        <Route path="/favoritos" element={<Favorites />} />
+        <Route path="/perfil" element={<Profile />} />
+        <Route path="/admin" element={<AdminPanel />} />
       </Routes>
     </>
   );
