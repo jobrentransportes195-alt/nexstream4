000baf316b61d52ef8adf0ecd41186b9c637289b
nexstrem2 <jobrentransportes195@gmail.com>
2026-02-19 18:09:35 -0300

    Atualizar o content.ts

--- data/content.ts
+++ data/content.ts
@@ -1,1 +1,16 @@
-y
+export const content = [
+  {
+    title: "Avatar",
+    category: "Filmes",
+    image: "https://image.tmdb.org/t/p/w500/jRXYjXNq0Cs2TcJjLkki24MLp7u.jpg",
+    video: "https://www.w3schools.com/html/mov_bbb.mp4",
+    rating: 5
+  },
+  {
+    title: "Breaking Bad",
+    category: "Séries",
+    image: "https://image.tmdb.org/t/p/w500/eSzpy96DwBujGFj0xMbXBcGcfxX.jpg",
+    video: "https://www.w3schools.com/html/movie.mp4",
+    rating: 5
+  }
+];
