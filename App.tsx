5590d7fb486b5e9ee8e96c13a4ac8cdd1980aa5c
nexstrem2 <jobrentransportes195@gmail.com>
2026-02-19 18:19:10 -0300

    Atualizar o App.tsx

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import Login from "./components/Login";
 
 export default function App() {
   const [user, setUser] = useState<any>(null);
+  const [selectedCategory, setSelectedCategory] = useState("Home");
 
   if (!user) {
     return <Login onLogin={setUser} />;
@@ -12,16 +13,23 @@ export default function App() {
   return (
     <>
       <div className="navbar">
-        <h2 style={{color:"#e50914"}}>NexStream</h2>
-        <button>Home</button>
-        <button>TV Ao Vivo</button>
-        <button>Filmes</button>
-        <button>Séries</button>
-        <button>Minha Lista</button>
+        <h2 style={{ color: "#e50914" }}>NexStream</h2>
+
+        <button onClick={() => setSelectedCategory("Home")}>Home</button>
+        <button onClick={() => setSelectedCategory("TV Ao Vivo")}>
+          TV Ao Vivo
+        </button>
+        <button onClick={() => setSelectedCategory("Filmes")}>
+          Filmes
+        </button>
+        <button onClick={() => setSelectedCategory("Séries")}>
+          Séries
+        </button>
+
         <button onClick={() => setUser(null)}>Sair</button>
       </div>
 
-      <Home />
+      <Home category={selectedCategory} />
     </>
   );
 }
