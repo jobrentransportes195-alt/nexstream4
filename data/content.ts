6ef57e0820b9dfae2588dab3115dcaac81a59cb4
nexstrem2 <jobrentransportes195@gmail.com>
2026-02-19 18:32:01 -0300

    Atualizar o content.ts

--- data/content.ts
+++ data/content.ts
@@ -1,49 +1,20 @@
 export const content = [
-  // FILMES
   {
+    id: "avatar",
     title: "Avatar",
     category: "Filmes",
+    description: "Um ex-fuzileiro é enviado para Pandora.",
     image: "https://image.tmdb.org/t/p/w500/6EiRUJpuoeQPghrs3YNktfnqOVh.jpg",
     video: "https://www.w3schools.com/html/mov_bbb.mp4",
     rating: 5
   },
   {
-    title: "Batman",
-    category: "Filmes",
-    image: "https://image.tmdb.org/t/p/w500/74xTEgt7R36Fpooo50r9T25onhq.jpg",
-    video: "https://www.w3schools.com/html/movie.mp4",
-    rating: 4
-  },
-
-  // SÉRIES
-  {
+    id: "breakingbad",
     title: "Breaking Bad",
     category: "Séries",
+    description: "Um professor vira fabricante de metanfetamina.",
     image: "https://image.tmdb.org/t/p/w500/ggFHVNu6YYI5L9pCfOacjizRGt.jpg",
     video: "https://www.w3schools.com/html/movie.mp4",
     rating: 5
-  },
-  {
-    title: "Dark",
-    category: "Séries",
-    image: "https://image.tmdb.org/t/p/w500/7MIlTMEj6W1W6dPjKxwXH6EXV9M.jpg",
-    video: "https://www.w3schools.com/html/movie.mp4",
-    rating: 4
-  },
-
-  // TV AO VIVO
-  {
-    title: "Globo",
-    category: "TV Ao Vivo",
-    image: "https://upload.wikimedia.org/wikipedia/commons/5/5f/TV_Globo_logo_2021.svg",
-    video: "https://www.w3schools.com/html/mov_bbb.mp4",
-    rating: 5
-  },
-  {
-    title: "SBT",
-    category: "TV Ao Vivo",
-    image: "https://upload.wikimedia.org/wikipedia/commons/0/0d/SBT_logo_2014.svg",
-    video: "https://www.w3schools.com/html/mov_bbb.mp4",
-    rating: 4
   }
 ];
