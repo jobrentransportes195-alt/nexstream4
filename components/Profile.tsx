478d2be3856ede041c0bef19278bf08b08f4f5c8
nexstrem2 <jobrentransportes195@gmail.com>
2026-02-19 19:21:59 -0300

    Criar Profile.tsx

---
+++ components/Profile.tsx
@@ -0,0 +1,9 @@
+export default function Profile() {
+  return (
+    <div className="page-container">
+      <h2>Perfil do Usuário</h2>
+      <p>Usuário Pro</p>
+      <p>Email: usuario@email.com</p>
+    </div>
+  );
+}
