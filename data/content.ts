530eac49cceb9d52270c55047f5b2d352846278b
nexstrem2 <jobrentransportes195@gmail.com>
2026-02-19 18:28:37 -0300

    Atualizar o content.ts

--- data/content.ts
+++ data/content.ts
@@ -18,4 +18,32 @@ export const content = [
   // SÉRIES
   {
     title: "Breaking Bad",
-    category
+    category: "Séries",
+    image: "https://image.tmdb.org/t/p/w500/ggFHVNu6YYI5L9pCfOacjizRGt.jpg",
+    video: "https://www.w3schools.com/html/movie.mp4",
+    rating: 5
+  },
+  {
+    title: "Dark",
+    category: "Séries",
+    image: "https://image.tmdb.org/t/p/w500/7MIlTMEj6W1W6dPjKxwXH6EXV9M.jpg",
+    video: "https://www.w3schools.com/html/movie.mp4",
+    rating: 4
+  },
+
+  // TV AO VIVO
+  {
+    title: "Globo",
+    category: "TV Ao Vivo",
+    image: "https://upload.wikimedia.org/wikipedia/commons/5/5f/TV_Globo_logo_2021.svg",
+    video: "https://www.w3schools.com/html/mov_bbb.mp4",
+    rating: 5
+  },
+  {
+    title: "SBT",
+    category: "TV Ao Vivo",
+    image: "https://upload.wikimedia.org/wikipedia/commons/0/0d/SBT_logo_2014.svg",
+    video: "https://www.w3schools.com/html/mov_bbb.mp4",
+    rating: 4
+  }
+];
