038234b09b883207501f8ab05a8eca41f1c78c19
nexstrem2 <jobrentransportes195@gmail.com>
2026-02-19 20:04:24 -0300

    Atualizar o supabase.ts

--- services/supabase.ts
+++ services/supabase.ts
@@ -1,6 +1,9 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = 'https://twdcflmtsganutzmjald.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR3ZGNmbG10c2dhbnV0em1qYWxkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc3OTM5NjMsImV4cCI6MjA4MzM2OTk2M30.C7AE5qDHGStXKcv3uS2fNagF1ycqyDEwFTSZlU9IkVA';
+const supabaseUrl = "SUA_URL";
+const supabaseAnonKey = "SUA_ANON_KEY";
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(
+  supabaseUrl,
+  supabaseAnonKey
+);
