a0f2528a2bcd0f15ebe50388f51c8237d98eab29
nexstrem2 <jobrentransportes195@gmail.com>
2026-02-19 23:08:18 -0300

    Atualizar o Player.tsx

--- components/Player.tsx
+++ components/Player.tsx
@@ -3,10 +3,12 @@ import { useParams } from "react-router-dom";
 function Player() {
   const { url } = useParams();
 
+  if (!url) return <div>Erro ao carregar canal</div>;
+
   return (
     <div style={{ background: "black", height: "100vh" }}>
       <video
-        src={decodeURIComponent(url!)}
+        src={decodeURIComponent(url)}
         controls
         autoPlay
         style={{ width: "100%", height: "100%" }}
