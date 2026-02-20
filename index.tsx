cb58b815ded45308d5cca39e3d34a399eddf6627
nexstrem2 <jobrentransportes195@gmail.com>
2026-02-20 14:00:57 -0300

    Atualizar o index.tsx

--- index.tsx
+++ index.tsx
@@ -2,21 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import "./index.css";
 
-const root = document.getElementById("root");
-
-if (!root) {
-  throw new Error("Root element not found");
-}
-
-ReactDOM.createRoot(root).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
 );
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("/sw.js");
-}
