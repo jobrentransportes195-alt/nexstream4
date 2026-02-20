122d17acebfd64e303f628cf3979ddcc693b14f8
nexstrem2 <jobrentransportes195@gmail.com>
2026-02-19 23:13:23 -0300

    Atualizar o App.tsx

--- App.tsx
+++ App.tsx
@@ -1,44 +1,7 @@
-import { Routes, Route } from "react-router-dom";
-import { useState, useEffect } from "react";
-import { supabase } from "./services/supabase";
-
-import Header from "./components/Header";
-import Home from "./components/Home";
-import Login from "./components/Login";
-import Player from "./components/Player";
-import AdminPanel from "./components/AdminPanel";
-
 function App() {
-  const [user, setUser] = useState<any>(null);
-
-  useEffect(() => {
-    supabase.auth.getSession().then(({ data }) => {
-      setUser(data.session?.user ?? null);
-    });
-
-    supabase.auth.onAuthStateChange((_event, session) => {
-      setUser(session?.user ?? null);
-    });
-  }, []);
-
-  async function handleLogout() {
-    await supabase.auth.signOut();
-    setUser(null);
-  }
-
-  if (!user) return <Login onLogin={setUser} />;
-
   return (
-    <>
-      <Header onLogout={handleLogout} />
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/player/:url" element={<Player />} />
-        <Route path="/admin" element={<AdminPanel />} />
-      </Routes>
-    </>
+    <div style={{ background: "red", height: "100vh" }}>
+      TESTE TEMA
+    </div>
   );
 }
-
-export default App;
